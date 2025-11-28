77034891b0b3a9ff9fdaf76d4a2189144063299a
Abdoul03 <samabdoul03@gmail.com>
2025-11-28 16:48:34 +0000

    ajout de api service

---
+++ src/app/models/user.ts
@@ -0,0 +1,16 @@
+export interface Fournisseur {
+  id: number;
+  nom: string;
+  prenom: string;
+  username: string;
+  email: string;
+  telephone: string;
+}
+export interface Commercant {
+  id: number;
+  nom: string;
+  prenom: string;
+  username: string;
+  email: string;
+  telephone: string;
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environement } from '../env';
+import { Commercant, Fournisseur } from '../models/user';
+import { Categorie } from '../models/categorie';
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiServices {
+  constructor(private htt: HttpClient) {}
+
+  getCommercant() {
+    return this.htt.get<Commercant[]>(`${environement.apiUrl}/commercant`);
+  }
+
+  getFournisseur() {
+    return this.htt.get<Fournisseur[]>(`${environement.apiUrl}/fournisseur`);
+  }
+
+  ajoutCategorie(nom: string) {
+    return this.htt.post<Categorie>(`${environement.apiUrl}/categorie`, { nom });
+  }
+
+  getCategorie() {
+    return this.htt.get<Categorie[]>(`${environement.apiUrl}/categorie`);
+  }
+
+  getCommande() {
+    return this.htt.get(`${environement.apiUrl}/commandeGroupee`);
+  }
+}
