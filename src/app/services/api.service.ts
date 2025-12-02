ef142df2f1b2b17d048e154ca43d050f6953261b
Abdoul03 <samabdoul03@gmail.com>
2025-12-02 11:45:11 +0000

    Dynamisation de la page d'accuil

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,35 +4,40 @@ import { environement } from '../env';
 import { Commercant, Fournisseur } from '../models/user';
 import { Categorie } from '../models/categorie';
 import { CommandeGroupee } from '../models/commandeGroupee';
-import { ProduitCount } from './produit.service';
 import { Produits } from '../models/produits';
+import { Observable } from 'rxjs';
+import { Transaction } from '../models/transaction';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServices {
   constructor(private http: HttpClient) {}
 
-  getCommercant() {
+  getCommercant(): Observable<Commercant[]> {
     return this.http.get<Commercant[]>(`${environement.remoteUrl}/commercant`);
   }
 
-  getFournisseur() {
+  getFournisseur(): Observable<Fournisseur[]> {
     return this.http.get<Fournisseur[]>(`${environement.remoteUrl}/fournisseur`);
   }
 
-  ajoutCategorie(nom: string) {
+  ajoutCategorie(nom: string): Observable<Categorie> {
     return this.http.post<Categorie>(`${environement.remoteUrl}/categorie`, { nom });
   }
 
-  getCategorie() {
+  getCategorie(): Observable<Categorie[]> {
     return this.http.get<Categorie[]>(`${environement.remoteUrl}/categorie`);
   }
 
-  getCommande() {
+  getCommande(): Observable<CommandeGroupee[]> {
     return this.http.get<CommandeGroupee[]>(`${environement.remoteUrl}/commandeGroupee`);
   }
 
-  getProduit() {
-    return this.http.get<Produits>(`${environement.remoteUrl}/produits`);
+  getProduit(): Observable<Produits[]> {
+    return this.http.get<Produits[]>(`${environement.remoteUrl}/produits`);
+  }
+
+  getTransactions(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(`${environement.remoteUrl}/transaction`);
   }
 }
